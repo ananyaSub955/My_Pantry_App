8cbb377d55e926478e60e7f830f1664d57291ea8
ananaySub955 <ananyasub955@gmail.com>
2024-06-30 16:50:38 -0700

    making the login and sign up page and connecting them to each other, also deleted the tutorial doc

--- lib/home.dart
+++ lib/home.dart
@@ -1,43 +1,43 @@
-import 'package:flutter/material.dart';
-import 'package:pantry_app/coffeeprefs.dart';
+// import 'package:flutter/material.dart';
+// import 'package:pantry_app/coffeeprefs.dart';
 
 
-//creates a new class for me
-class Home extends StatelessWidget {
-  const Home({super.key}); //constructor
+// //creates a new class for me
+// class Home extends StatelessWidget {
+//   const Home({super.key}); //constructor
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("my login"),
-        backgroundColor: Colors.pink[100],
-        centerTitle: true,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            color: Colors.pink[300],
-            padding: const EdgeInsets.all(20),
-            child: const Text("my name is ananya", style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold, 
-            )),
-          ),
-          Container(
-            color: Colors.purple[300],
-            padding: const EdgeInsets.all(20),
-            child: const CoffeePrefs(),
-          ),
-          Expanded(
-            child: Image.asset('assets/img/b2.jpeg',
-              fit: BoxFit.fitWidth,
-              alignment: Alignment.bottomCenter,
-            ),
-          ),
-        ]
-        ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("my login"),
+//         backgroundColor: Colors.pink[100],
+//         centerTitle: true,
+//       ),
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           Container(
+//             color: Colors.pink[300],
+//             padding: const EdgeInsets.all(20),
+//             child: const Text("my name is ananya", style: TextStyle(
+//               color: Colors.white,
+//               fontWeight: FontWeight.bold, 
+//             )),
+//           ),
+//           Container(
+//             color: Colors.purple[300],
+//             padding: const EdgeInsets.all(20),
+//             child: const CoffeePrefs(),
+//           ),
+//           Expanded(
+//             child: Image.asset('assets/img/b2.jpeg',
+//               fit: BoxFit.fitWidth,
+//               alignment: Alignment.bottomCenter,
+//             ),
+//           ),
+//         ]
+//         ),
+//     );
+//   }
+// }
